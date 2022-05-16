ef9cd8f5157a59f515c10bdd9ebf79b51615e85b
pkinn <pkinn@umich.edu>
2022-05-16 11:31:05 -0400

    Updating KNN code
    Fixed error where 8000 was used as the end of emi_data.iloc[:, 3:8000]; should just be [:,3:] (from 3rd index to the last index)

--- physchem_models.py
+++ physchem_models.py
@@ -44,7 +44,7 @@ igg_psy_transform = pd.DataFrame(-1*lda_psy.transform(igg_physvh)).set_index(igg
 
 
 #%%
-"""
+
 # sample size elbow plot
 emi_data = pd.concat([emi_binding, emi_physvh.set_index(emi_binding.index)], axis = 1)
 ant_test_acc = []
@@ -66,14 +66,14 @@ plt.yticks([0.5, 0.6, 0.7,0.8, 0.9, 1.0], [50, 60, 70, 80, 90, 100], fontsize = 
 #%%
 #KNN of sequences
 from sklearn.neighbors import KNeighborsClassifier as KNC
-
+emi_data = pd.concat([emi_binding, emi_physvh.set_index(emi_binding.index)], axis = 1)
 ant_predict_acc = []
 psy_predict_acc = []
 for j in np.arange(1,25):
     knc = KNC(n_neighbors = j)
-    cv_results = cv(knc, emi_data.iloc[:,3:8000], emi_data.iloc[:,0])
+    cv_results = cv(knc, emi_data.iloc[:,3:], emi_data.iloc[:,0])
     ant_predict_acc.append(np.mean(cv_results['test_score']))
-    cv_results = cv(knc, emi_data.iloc[:,3:8000], emi_data.iloc[:,1])
+    cv_results = cv(knc, emi_data.iloc[:,3:], emi_data.iloc[:,1])
     psy_predict_acc.append(np.mean(cv_results['test_score']))
     
 #%%
@@ -81,7 +81,7 @@ plt.scatter(np.arange(1,25), ant_predict_acc, c = 'red', edgecolor = 'k', linewi
 plt.scatter(np.arange(1,25), psy_predict_acc, c = 'blue', edgecolor = 'k', linewidth = 0.25, s = 50)
 plt.xticks(fontsize = 24)
 plt.yticks([0.8, 0.9, 1.0], [80, 90, 100], fontsize = 24)
-"""
+
 #%%
 #model accuracy distributions
 plt.figure()
